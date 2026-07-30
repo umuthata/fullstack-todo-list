b4712b7145543f2b719377b63c583a73f99f38d8
Umut Hata <umut.hata59@gmail.com>
2026-07-30 16:47:40 +0300

    Canlı API bağlantısı düzeltildi

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:
-        import.meta.env.VITE_API_URL ??
-        "http://localhost:8080/api",
+    baseURL: import.meta.env.PROD
+        ? "https://todo-app-backend-7pv4.onrender.com/api"
+        : "http://localhost:8080/api",
 
     headers: {
         "Content-Type": "application/json",
